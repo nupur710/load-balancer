f17c2975b49e434a59b40215548fa67e034ce38f
Nupur Singh <nupur.singh2410@gmail.com>
2024-06-06 22:42:30 +0530

    simple server-client

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+
+    static Socket socket= null;
+    static DataInputStream input= null;
+    static DataOutputStream output= null;
+
+    public static void main(String[] args) {
+        try {
+            socket = new Socket("localhost", 8888);
+            output= new DataOutputStream(socket.getOutputStream());
+            input= new DataInputStream(socket.getInputStream());
+            output.writeUTF("hello from cient");
+            String response= input.readUTF();
+            System.out.println(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
