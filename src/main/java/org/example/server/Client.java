3b8a9c038352c10c55084c21436fdc5c5132be99
Nupur Singh <nupur.singh2410@gmail.com>
2024-07-09 01:02:50 +0530

    send request by curl

--- src/main/java/org/example/server/Client.java
+++ src/main/java/org/example/server/Client.java
@@ -1,57 +1,32 @@
 package org.example.server;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 public class Client implements Runnable {
-
-    private Socket socket;
-    private DataInputStream res;
-    private DataOutputStream req;
-    private static int counter= 1;
-    private final int requestId;
-
-    public Client(int port) throws IOException {
-        //Connect to load balancer port
-        socket= new Socket("localhost", port);
-        synchronized (Client.class) {
-            requestId= counter++;
-        }
+    ProcessBuilder processBuilder;
+    Process process;
+    public Client(ProcessBuilder processBuilder, Process process)  {
+        this.process= process;
+        this.processBuilder= processBuilder;
     }
 
     @Override
     public void run() {
         try {
-            req = new DataOutputStream(socket.getOutputStream());
-            res= new DataInputStream(socket.getInputStream());
-            req.writeUTF("Request " + requestId);
-            ByteArrayOutputStream bufferResp= new ByteArrayOutputStream();
-            byte[] buffer= new byte[8192];
-            int bytesRead;
-            while((bytesRead= res.read(buffer)) != -1) {
-                bufferResp.write(buffer, 0, bytesRead);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line).append("\n");
             }
-            String responseMessage = bufferResp.toString();
-            System.out.println("Response: " + responseMessage);
+            System.out.println(response);
+            processBuilder.command(
+                    new String[]{"curl", "http://localhost:1221"});
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (req != null) {
-                    req.close();
-                    if (socket != null) {
-                        socket.close();
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
-
     }
-
 }
--- src/main/java/org/example/server/SendRequest.java
+++ src/main/java/org/example/server/SendRequest.java
@@ -1,14 +1,25 @@
 package org.example.server;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SendRequest {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         while (true) {
-            Client client1 = new Client(1221);
+            String cmd= "curl http://localhost:1221";
+            ProcessBuilder processBuilder= new ProcessBuilder(cmd.split(" "));
+            processBuilder.directory(new File("\\Users\\lenovo"));
+            Process process = processBuilder.start();
+            Client client1 = new Client(processBuilder, process);
             Thread requests = new Thread(client1);
             requests.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+            } else {
+                System.err.println("Curl command failed with exit code " + exitCode);
+            }
+            process.destroy();
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
