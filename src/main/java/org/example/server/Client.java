d742a7a404daf5c2d2f821cafc10a1939f086cb9
Nupur Singh <nupur.singh2410@gmail.com>
2024-07-08 23:22:23 +0530

    client fixes

--- src/main/java/org/example/server/Client.java
+++ src/main/java/org/example/server/Client.java
@@ -1,15 +1,12 @@
 package org.example.server;
 
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Client implements Runnable {
-
-    static class Request {
-
-    }
 
     private Socket socket;
     private DataInputStream res;
@@ -29,7 +26,17 @@ public class Client implements Runnable {
     public void run() {
         try {
             req = new DataOutputStream(socket.getOutputStream());
+            res= new DataInputStream(socket.getInputStream());
             req.writeUTF("Request " + requestId);
+            ByteArrayOutputStream bufferResp= new ByteArrayOutputStream();
+            byte[] buffer= new byte[8192];
+            int bytesRead;
+            while((bytesRead= res.read(buffer)) != -1) {
+                bufferResp.write(buffer, 0, bytesRead);
+            }
+            String responseMessage = bufferResp.toString();
+            System.out.println("Response: " + responseMessage);
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
