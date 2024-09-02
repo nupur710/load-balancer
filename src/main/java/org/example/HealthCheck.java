a793b6262880078c48ed13e282a7c05c70e440f8
Nupur Singh <nupur.singh2410@gmail.com>
2024-09-02 23:07:23 +0530

    health check servers

---
+++ src/main/java/org/example/HealthCheck.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HealthCheck implements Runnable {
+    private final List<Server> ports;
+    private final List<Server> healthyServer;
+    HealthCheck(List<Server> ports) {
+        this.ports= ports;
+        healthyServer= new ArrayList<>();
+    }
+    @Override
+    public void run() {
+        for (Server s1 : ports) {
+            if(isServerHealthy(s1)) {
+                healthyServer.add(s1);
+            }
+        }
+    }
+    private boolean isServerHealthy(Server server) {
+        try {
+            Process process = new ProcessBuilder("curl", "-I", "http://localhost:" + server.getPort()) //http head request
+                    .redirectErrorStream(true).start();
+            int exitCode= process.waitFor();
+            return exitCode== 0;
+        } catch (Exception e) {
+            System.err.println("Error sending HTTP HEAD request to check health of server");
+            return false;
+        }
+    }
+}
