0b4326b545cacad8496b992b4c1952cfccfc29aa
Nupur Singh <nupur.singh2410@gmail.com>
2024-09-09 23:56:53 +0530

    LB should handle requests concurrently

--- src/main/java/org/loadbalancer/LoadBalancer.java
+++ src/main/java/org/loadbalancer/LoadBalancer.java
@@ -4,11 +4,16 @@ import org.loadbalancer.algorithms.LoadBalancerStrategy;
 import org.loadbalancer.algorithms.WeightedRoundRobin;
 import org.loadbalancer.server.Server;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class LoadBalancer {
     static private int serversStartPort = 8080;
@@ -26,38 +31,46 @@ public class LoadBalancer {
         healthCheck.startScheduler();
 
         try (ServerSocket loadBalancer = new ServerSocket(1221)) {
+            //Handle incoming requests in multiple threads
+            ExecutorService threadPool = Executors.newFixedThreadPool(10);
             while (true) {
-                try (Socket lbSocket = loadBalancer.accept();
-                     DataInputStream lbClient = new DataInputStream(lbSocket.getInputStream());
-                     DataOutputStream lbClientOutput = new DataOutputStream(lbSocket.getOutputStream())) {
-                    // Refresh the list of healthy servers for every client request
-                    List<Server> healthyServers = healthCheck.getHealthyServers();
-                    if (healthyServers.isEmpty()) {
-                        System.err.println("No healthy server available");
-                        lbClientOutput.writeUTF("503 Service Unavailable: No healthy servers");
-                        continue;
-                    }
-                    Server selectedServer = strategy.selectServer(healthyServers);
-                    try (Socket connectToServer = new Socket("localhost", selectedServer.getPort());
-                         DataOutputStream lbServer = new DataOutputStream(connectToServer.getOutputStream());
-                         DataInputStream lbServerResp = new DataInputStream(connectToServer.getInputStream())) {
-
-                        // Send client request to selected server
-                        int bytesRead = sendRequest(lbClient, lbServer);
-
-                        // Read response from server and send it back to client
-                        readResponse(lbClientOutput, lbServerResp, bytesRead);
-
-                    } catch (IOException e) {
-                        System.err.println("Error connecting to server: " + e.getMessage());
-                    }
-                } catch (IOException e) {
-                    System.err.println("Error handling client connection: " + e.getMessage());
-                }
+                Socket lbSocket = loadBalancer.accept();
+                threadPool.submit(() -> {
+                    handleRequest(lbSocket, healthCheck);
+                });
             }
         } catch (IOException e) {
             System.err.println("Error in load balancer: " + e.getMessage());
         }
+    }
+
+    private static void handleRequest(Socket lbSocket, HealthCheck healthCheck) {
+        try (DataInputStream lbClient= new DataInputStream(lbSocket.getInputStream());
+            DataOutputStream lbClientOutput= new DataOutputStream(lbSocket.getOutputStream());) {
+            // Refresh the list of healthy servers for every client request
+            List<Server> healthyServers = healthCheck.getHealthyServers();
+            if (healthyServers.isEmpty()) {
+                System.err.println("No healthy server available");
+                lbClientOutput.writeUTF("503 Service Unavailable: No healthy servers");
+                return;
+            }
+            Server selectedServer = strategy.selectServer(healthyServers);
+            try (Socket connectToServer = new Socket("localhost", selectedServer.getPort());
+                 DataOutputStream lbServer = new DataOutputStream(connectToServer.getOutputStream());
+                 DataInputStream lbServerResp = new DataInputStream(connectToServer.getInputStream())) {
+
+                // Send client request to selected server
+                int bytesRead = sendRequest(lbClient, lbServer);
+
+                // Read response from server and send it back to client
+                readResponse(lbClientOutput, lbServerResp, bytesRead);
+
+            } catch (IOException e) {
+                System.err.println("Error connecting to server: " + e.getMessage());
+            }
+        } catch (IOException e) {
+            System.err.println("Error handling client connection: " + e.getMessage());
+            }
     }
 
     private static int sendRequest(DataInputStream dis, DataOutputStream dos) throws IOException {
@@ -70,13 +83,13 @@ public class LoadBalancer {
     }
 
     private static void readResponse(DataOutputStream dos, DataInputStream dis, int bytesRead) throws IOException {
-        ByteArrayOutputStream responseBuffer = new ByteArrayOutputStream();
-        while ((bytesRead = dis.read(buffer)) != -1) {
-            responseBuffer.write(buffer, 0, bytesRead);
-        }
-        String responseFromServer = responseBuffer.toString();
-        System.out.println("Resp received from server:\n" + responseFromServer);
-        dos.write(responseBuffer.toByteArray());
-        dos.flush();
+            ByteArrayOutputStream responseBuffer = new ByteArrayOutputStream();
+            while ((bytesRead = dis.read(buffer)) != -1) {
+                responseBuffer.write(buffer, 0, bytesRead);
+            }
+            String responseFromServer = responseBuffer.toString();
+            System.out.println("Resp received from server:\n" + responseFromServer);
+            dos.write(responseBuffer.toByteArray());
+            dos.flush();
     }
 }
--- src/main/java/org/loadbalancer/algorithms/LoadBalancerStrategy.java
+++ src/main/java/org/loadbalancer/algorithms/LoadBalancerStrategy.java
@@ -3,7 +3,7 @@ package org.loadbalancer.algorithms;
 import org.loadbalancer.server.Server;
 
 import java.util.List;
-
+//Strategy design pattern
 public interface LoadBalancerStrategy {
     Server selectServer(List<Server> healthyServers);
 }
