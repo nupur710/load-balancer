f89f67987037b8808bc266cdeaaf84ef985c92a7
Nupur Singh <nupur.singh2410@gmail.com>
2024-09-10 22:24:03 +0530

    use ThreadPool

--- src/main/java/org/loadbalancer/client/SendRequest.java
+++ src/main/java/org/loadbalancer/client/SendRequest.java
@@ -2,18 +2,20 @@ package org.loadbalancer.client;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SendRequest {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        ExecutorService exec= Executors.newFixedThreadPool(5);
         while (true) {
             String cmd= "curl http://localhost:1221";
             ProcessBuilder processBuilder= new ProcessBuilder(cmd.split(" "));
             processBuilder.directory(new File("\\Users\\lenovo"));
             Process process = processBuilder.start();
             Client client1 = new Client(process);
-            Thread requests = new Thread(client1);
-            requests.start();
+            exec.submit(client1);
             int exitCode = process.waitFor();
             if (exitCode != 0) {
                 System.err.println("Curl command failed with exit code " + exitCode);
