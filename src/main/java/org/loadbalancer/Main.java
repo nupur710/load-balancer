030de78ca68bf392c79a2fd1b9f60f9e23dad058
Nupur Singh <nupur.singh2410@gmail.com>
2024-09-12 01:05:45 +0530

    update dependencies

---
+++ src/main/java/org/loadbalancer/Main.java
@@ -0,0 +1,20 @@
+package org.loadbalancer;
+
+import org.loadbalancer.server.MultipleServers;
+
+/**
+ * We start both the servers and load balancer from the same process (same jvm).
+ *  Previously when running them as different programs meant they have different
+ *  memory spaces i.e., when servers were created from MultipleServers.java
+ *  & stored server objs in ConcurrentHashMap serverObj, they were not accessible to
+ *  LoadBalancer.java class
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        // Start servers
+        new Thread(() -> MultipleServers.main(args)).start();
+        Thread.sleep(100);
+        // Start load balancer
+        new Thread(() -> LoadBalancer.main(args)).start();
+    }
+}
